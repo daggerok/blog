c269d3ec9baac4f858668498ca885b181d7c9a9d
Maksim Kostromin <daggerok@gmail.com>
2015-11-05 06:14:06 +0200

    reviwe, remove comments and so on

--- src/main/resources/ui/npm.bat
+++ src/main/resources/ui/npm.bat
@@ -1,4 +1,7 @@
 @echo off
 
+@rem npm run npm-install --prefix=src\main\resources\ui
 @rem npm run gulp --prefix=src\main\resources\ui
+
+npm run npm-install
 npm run gulp
---
+++ src/main/resources/ui/npm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#npm run npm-install --prefix=src/main/resources/ui
+#npm run gulp --prefix=src/main/resources/ui
+
+npm run npm-install
+npm run gulp

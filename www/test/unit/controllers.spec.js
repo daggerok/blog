f7fc6d618fbc653973dd2dd1253bd0a57c301d20
Maksim Kostromin <daggerok@gmail.com>
2015-11-10 04:07:41 +0200

    dome with karma tests

--- www/test/unit/controllers.spec.js
+++ www/test/unit/controllers.spec.js
@@ -3,9 +3,8 @@
 /**
  * Created by mak on 11/10/15.
  */
-
 describe('Testing blog controllers', function() {
   it("and has a positive case", function() {
-    expect(true).toBe(false);
+    expect(true).toBe(true);
   });
 });

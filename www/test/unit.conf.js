08149e47734979d05d77efa1919075fd8215944c
Maksim Kostromin <daggerok@gmail.com>
2015-11-10 03:26:29 +0200

    add karms-jasmine tests

--- www/test/unit.conf.js
+++ www/test/unit.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'src/js/**/*.js',
-      'test/unit/**/*.js'
+      'test/unit/**/*.spec.js'
     ],
 
     // list of files to exclude

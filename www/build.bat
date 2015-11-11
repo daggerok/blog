2e81c5ba30cbcc608f90e57e6094c00972de969a
Maksim Kostromin <Maksim.Kostromin@sigma.software>
2015-11-11 17:37:37 +0200

    fix build.bat review index.html

--- www/build.bat
+++ www/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call npm run clean
 call npm run fetch
+call npm run clean
 call npm run gulp
 call npm run win-test
---
+++ www/dev.bat
@@ -0,0 +1,4 @@
+@echo off
+
+call npm run clean
+call npm run watch

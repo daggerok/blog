3856c35c5ac3efb2a1d58aebb53137e3a533c63d
Maksim Kostromin <Maksim.Kostromin@sigma.software>
2015-11-12 13:45:52 +0200

    add html/css/js commpression

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -11,18 +11,14 @@ const srcDir = 'src/',
   webDir = '../src/main/resources/public/';
 
 const gulp = require('gulp'),
-  rm = require('gulp-rimraf'),
   combine = require('gulp-concat'),
   plumber = require('gulp-plumber'),
-  connect = require('gulp-connect'),
-  compress = require('gulp-gzip'),
-  rename = require("gulp-rename");
+  connect = require('gulp-connect');
 
 gulp.task('connect', function() { // livereoad 1
   connect.server({
     root: webDir,
-    livereload: true
-  });
+    livereload: true});
 });
 
 // run compiled sources
@@ -41,7 +37,7 @@ gulp.task('clean', function() {
   return gulp
     .src([webDir], {read: false})
     .pipe(plumber())
-    .pipe(rm({force: true}));
+    .pipe(require('gulp-rimraf')({force: true}));
 });
 
 // vendors to libs/js
@@ -62,15 +58,15 @@ gulp.task('scripts', ['js-vendor'], function() {
     srcDir + mainJs,
     srcDir + 'js/directives.js'
   ];
-  // zip all to mainJs
+  // combine and min all to mainJs
   return gulp
+    // emitting errors without plumber:
+    //.on('error', console.error.bind(console))
     .src(scripts)
     .pipe(plumber())
     .pipe(combine(mainJs))
     .pipe(plumber())
     .pipe(require('gulp-uglify')())
-    // emitting errors without plumber:
-    //.on('error', console.error.bind(console))
     .pipe(gulp.dest(webDir))
     .pipe(connect.reload()); // livereoad 3
 });
@@ -80,9 +76,11 @@ gulp.task('css-vendor', function() {
   return gulp.src([
     modulesDir + 'font-awesome/css/font-awesome.css',
     modulesDir + 'bootstrap/dist/css/bootstrap.css'
-  ]).pipe(gulp.dest(libs + 'css/'))});
-// combine and min css files into build dir ?? ????????????
-gulp.task('uncompressed-styles', ['css-vendor'], function() {
+  ]).pipe(gulp.dest(libs + 'css/'));
+});
+
+// combine and min css files into build dir
+gulp.task('styles', ['css-vendor'], function() {
   var styles = [
     libs + 'css/font-awesome.css',
     libs + 'css/bootstrap.css',
@@ -90,44 +88,15 @@ gulp.task('uncompressed-styles', ['css-vendor'], function() {
   ];
   // zip all to mainCss
   return gulp
-    .src(styles)
-    .pipe(plumber())
+    .src(styles).pipe(plumber())
     .pipe(require('gulp-autoprefixer')())
     .pipe(plumber())
     .pipe(combine(mainCss))
     .pipe(plumber())
     .pipe(require('gulp-postcss')([require('csswring')({removeAllComments: true})]))
     .pipe(gulp.dest(webDir))
-    .pipe(connect.reload())}); // livereoad 3
-//rename according requirements
-gulp.task('rename-nogzip-styles', ['uncompressed-styles'], function() {
-  return gulp.src(webDir + mainCss)
-    .pipe(plumber())
-    .pipe(rename('blog.nogzip.css'))
-    .pipe(gulp.dest(webDir + 'css/'))});
-// compress full styles file
-gulp.task('compress-styles', ['rename-nogzip-styles'], function() {
-  return gulp.src(webDir + 'css/blog.nogzip.css')
-    .pipe(plumber())
-    .pipe(compress({
-      append: true,
-      extension: 'gz',
-      threshold: true,
-      gzipOptions: {
-        level: 9,
-        memLevel: 5
-      }}))
-    .pipe(gulp.dest(webDir + 'css/'))});
-// rename compressed file according requirements
-gulp.task('commpressed-to-styles', ['compress-styles'], function() {
-  return gulp.src(webDir + 'css/blog.nogzip.css.gz')
-    .pipe(plumber())
-    .pipe(rename(mainCss))
-    .pipe(gulp.dest(webDir))});
-// remove temp style file
-gulp.task('styles', ['commpressed-to-styles'], function() {
-  return gulp.src(webDir + 'css/blog.nogzip.css.gz')
-    .pipe(rm({force: true}))});
+    .pipe(connect.reload()); // livereoad 3
+});
 
 // min images into build dir
 gulp.task('images', function() {
